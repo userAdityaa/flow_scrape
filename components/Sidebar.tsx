f03f790c54c9df8a009f943e9dc0ce4910f34bc5
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-11-12 04:02:11 +0530

    modifed_sidebar: css components were modified

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -2,6 +2,9 @@
 import { CoinsIcon, HomeIcon, Layers2Icon, ShieldCheckIcon } from "lucide-react";
 import React from "react";
 import Logo from "./Logo";
+import Link from "next/link";
+import { buttonVariants } from "./ui/button";
+import { usePathname } from "next/navigation";
 
 const routes = [
     {
@@ -27,10 +30,24 @@ const routes = [
 ]
 
 function DesktopSidebar() { 
+    const pathName = usePathname();
+    const activeRoute =  routes.find((route) => route.href.length > 0 && pathName.includes(route.href)) || routes[0];
     return (
         <div className="hidden relative md:block min-w-[280px] max-w-[280px] h-screen overflow-hidden w-full bg-primary/5 dark:bg-secondary/30 dark:text-foreground text-muted-foreground border-r-2 border-separate">
             <div className="flex items-center justify-center gap-2 border-b-[1px] border-seperate p-4">
                 <Logo/>
+            </div>
+            <div className="flex flex-col p-2">
+                {routes.map(route => (
+                    <Link key = {route.href} href = {route.href} className={
+                        buttonVariants({
+                            variant: route.href === activeRoute.href ? "sidebarActiveItem" : "sidebarItem"
+                        })
+                    }> 
+                        <route.icon size={20} />
+                        {route.label}
+                    </Link>
+                ))}
             </div>
         </div>
     )
