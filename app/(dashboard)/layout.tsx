d68f43b82f2e209ffd7dbbef04c889c7cfff40eb
aditya.chaudhary1558@gmail.com <aditya.chaudhary1558@gmail.com>
2024-11-12 03:24:27 +0530

    add_dashboard: Added the dashboard folder for the UI

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,23 @@
+import { Separator } from '@/components/ui/separator'
+import React from 'react'
+
+function Layout({children}: {children: React.ReactNode}) { 
+    return (
+        <div className='flex h-screen'>
+            <DesktopSidebar />
+            <div className='flex flex-col flex-1 min-h-screen'>
+                <header className='flex items-center justify-between px-6 py-4 h-[50px] container'>
+                    ScrapeFlow
+                </header>
+                <Separator/>
+                <div className='overflow-auto'>
+                    <div className='flex-1 container py-4 text-accent-foreground'>
+                        {children}
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+} 
+
+ export default Layout;
